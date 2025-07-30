06cf9777e2869d2acacb1aba12fe001ca6597adc
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-29 21:56:58 -0600

    Initial Commit

---
+++ Shoping/ShopingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopingApp.swift
+//  Shoping
+//
+//  Created by Tirzaan on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ShopingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

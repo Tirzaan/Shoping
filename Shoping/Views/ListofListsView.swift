dc5b99d7168b7dd8e95b2951b9818b3181e031bf
Tirzaan Neil <Tirzaan@tntneil.com>
2025-08-11 19:46:39 -0600

    Added View Model to list of lists

--- Shoping/Views/ListofListsView.swift
+++ Shoping/Views/ListofListsView.swift
@@ -9,14 +9,16 @@ import SwiftUI
 
 struct ListofListsView: View {
     
-    @State var lists: [ListModel] = [
-        ListModel(name: "To Do", groups: [GroupModel(name: "", order: 0, items: [])]),
-        ListModel(name: "Grossery", groups: [
-            GroupModel(name: "Veggies", order: 1, items: []),
-            GroupModel(name: "Meat", order: 2, items: [])
-        ]),
-        ListModel(name: "Winco", groups: [GroupModel(name: "", order: 0, items: [])])
-    ]
+    @StateObject var viewModel = ShopingViewModel()
+    
+//    @State var lists: [ListModel] = [
+//        ListModel(name: "To Do", groups: [GroupModel(name: "", order: 0, items: [])]),
+//        ListModel(name: "Grossery", groups: [
+//            GroupModel(name: "Veggies", order: 1, items: []),
+//            GroupModel(name: "Meat", order: 2, items: [])
+//        ]),
+//        ListModel(name: "Winco", groups: [GroupModel(name: "", order: 0, items: [])])
+//    ]
     
     @State var showAlert: Bool = false
     @State var alertTitle: String = "Empty Alert"
@@ -24,12 +26,12 @@ struct ListofListsView: View {
     @State var selectedList: ListModel = ListModel(name: "", groups: [])
     
     init() {
-        selectedList = lists.first ?? ListModel(name: "", groups: [])
+        selectedList = viewModel.lists.first ?? ListModel(name: "", groups: [])
     }
     
     var body: some View {
         NavigationStack {
-            ListofLists(lists: $lists, showAlert: $showAlert, alertTitle: $alertTitle, alertMessage: $alertMessage, selectedList: $selectedList)
+            ListofLists(showAlert: $showAlert, alertTitle: $alertTitle, alertMessage: $alertMessage, selectedList: $selectedList)
             .alert(isPresented: $showAlert) { GetAlert() }
             
             .navigationTitle("Lists")
@@ -44,7 +46,7 @@ extension ListofListsView {
             message: Text(alertMessage),
             primaryButton: .cancel(),
             secondaryButton: .destructive(Text("DELETE")) {
-                lists.removeAll(where: { $0.name == selectedList.name })
+                viewModel.lists.removeAll(where: { $0.name == selectedList.name })
             },
         )
     }
@@ -52,7 +54,8 @@ extension ListofListsView {
 
 struct ListofLists: View {
     
-    @Binding var lists: [ListModel]
+    @StateObject var viewModel = ShopingViewModel()
+    
     @Binding var showAlert: Bool
     @Binding var alertTitle: String
     @Binding var alertMessage: String
@@ -61,7 +64,7 @@ struct ListofLists: View {
     
     var body: some View {
         List {
-            ForEach(lists) { list in
+            ForEach(viewModel.lists) { list in
                 NavigationLink {
                     ListView()
                 } label: {
@@ -100,13 +103,13 @@ struct ListofLists: View {
     }
     
     func moveItem(from source: IndexSet, to destination: Int) {
-        lists.move(fromOffsets: source, toOffset: destination)
+        viewModel.lists.move(fromOffsets: source, toOffset: destination)
     }
     
     func deleteButton(list: ListModel) {
         alertTitle = "Are You Sure You Want To Delete '\(list.name)' List?"
         alertMessage = "By clicking delete, this list will be permanently removed."
-        selectedList = lists.first(where: { $0.name == list.name }) ?? ListModel(name: "", groups: [])
+        selectedList = viewModel.lists.first(where: { $0.name == list.name }) ?? ListModel(name: "", groups: [])
         showAlert.toggle()
     }
     

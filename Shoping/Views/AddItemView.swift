81e71f71445074dd2e1cb0b522d7c8ed8056f4ec
Tirzaan Neil <Tirzaan@tntneil.com>
2025-08-11 22:15:17 -0600

    [Feature] Created Add Group and Add Item Views

---
+++ Shoping/Views/AddItemView.swift
@@ -0,0 +1,18 @@
+//
+//  AddItemView.swift
+//  Shoping
+//
+//  Created by Tirzaan on 8/11/25.
+//
+
+import SwiftUI
+
+struct AddItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddItemView()
+}

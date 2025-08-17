c9aecbc17291fe040be4c6c9eefcd134e713a33e
Tirzaan Neil <Tirzaan@tntneil.com>
2025-08-16 20:34:43 -0600

    [Feature] Added Navigate Functions and Empty View to List of Lists

--- Shoping/Views/ListofListsView.swift
+++ Shoping/Views/ListofListsView.swift
@@ -18,12 +18,18 @@ struct ListofListsView: View {
     
     @State var navigateToList: Bool = false
     @State var navigateToNewList: Bool = false
+    @State var navigateToSettings: Bool = false
     
     var body: some View {
         NavigationStack {
-            listofLists
+            if viewModel.lists.isEmpty {
+                EmptyView
+            } else {
+                listofLists
+            }
             NavigationLink("", destination: ListView(), isActive: $navigateToList).hidden()
             NavigationLink("", destination: AddListView(), isActive: $navigateToNewList).hidden()
+            NavigationLink("", destination: SettingsView(), isActive: $navigateToSettings).hidden()
             .alert(isPresented: $showAlert) { GetAlert() }
             .navigationTitle("Lists")
         }
@@ -31,25 +37,22 @@ struct ListofListsView: View {
 }
 
 //MARK: VIEWS
+
+// List of Lists
 extension ListofListsView {
     var listofLists: some View {
+        
         List {
             ForEach(viewModel.lists) { list in
                 Button {
-                    viewModel.currentList = list
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.01, execute: {
-                        navigateToList = true
-                    })
+                    navigateToListFunction(list: list)
                 } label: {
                     HStack {
                         Text(list.name)
                         Spacer()
                         Menu {
                             Button("Edit") {
-                                viewModel.isEditingList = true
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.01, execute: {
-                                    navigateToNewList = true
-                                })
+                                navigateToEditListFunction(list: list)
                             }
                             Button("Delete") {
                                 deleteItemAlert(list: list)
@@ -64,10 +67,6 @@ extension ListofListsView {
                 }
             }
             .onMove(perform: moveItem)
-            
-            Button("Add Apples to Test") {
-                viewModel.addItemToList(item: "Apples", list: "Test")
-            }
         }
         .onAppear {
             viewModel.isEditingList = false
@@ -76,10 +75,7 @@ extension ListofListsView {
         .toolbar {
             ToolbarItem(placement: .topBarTrailing) {
                 Button {
-                    viewModel.currentList = ListModel(name: "", groups: [])
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
-                        navigateToNewList = true
-                    })
+                    navigateToNewListFunction()
                 } label: {
                     Image(systemName: "plus")
                 }
@@ -91,7 +87,7 @@ extension ListofListsView {
             
             ToolbarItem(placement: .topBarTrailing) {
                 Button {
-                    
+                    navigateSettingsFunction()
                 } label: {
                     Image(systemName: "gear")
                 }
@@ -100,6 +96,57 @@ extension ListofListsView {
     }
 }
 
+// Empty View
+extension ListofListsView {
+    var EmptyView: some View {
+        VStack {
+            Text("No Lists Found!")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.primary)
+            
+            Button {
+                navigateToNewListFunction()
+            } label: {
+                Text("Create list")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .padding(.horizontal)
+                    .background(.blue)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+            }
+            
+            Spacer()
+        }
+        .onAppear {
+            viewModel.isEditingList = false
+            viewModel.isEditingGroup = false
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    navigateToNewListFunction()
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            
+            ToolbarItem(placement: .topBarLeading) {
+                EditButton()
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    navigateSettingsFunction()
+                } label: {
+                    Image(systemName: "gear")
+                }
+            }
+        }
+    }
+}
 
 
 //MARK: FUCTIONS
@@ -130,6 +177,38 @@ extension ListofListsView {
         alertMessage = "By clicking delete, this list will be permanently removed."
         selectedList = viewModel.lists.first(where: { $0.name == list.name }) ?? ListModel(name: "", groups: [])
         showAlert.toggle()
+    }
+}
+
+//Navigate Functions
+extension ListofListsView {
+    func navigateToListFunction(list: ListModel) {
+        viewModel.currentList = list
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
+            navigateToList = true
+        })
+    }
+    
+    func navigateToEditListFunction(list: ListModel) {
+        viewModel.isEditingList = true
+        viewModel.currentList = list
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
+            navigateToNewList = true
+        })
+    }
+    
+    func navigateToNewListFunction() {
+        viewModel.currentList = ListModel(name: "", groups: [])
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
+            navigateToNewList = true
+        })
+    }
+    
+    func navigateSettingsFunction() {
+        viewModel.currentList = ListModel(name: "", groups: [])
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
+            navigateToSettings = true
+        })
     }
 }
 
